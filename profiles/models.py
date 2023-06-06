7ac62ef57cf567cd9b6c1f9ef81fb8809faa2ced
Stepan260 <koroid20081@gmail.com>
2023-06-07 00:04:08 +0300

    добавил модели, спарсил токен,занес в бд хуйня полная не смотри удали нахуй +++ pro max ultra tank assin

--- profiles/models.py
+++ profiles/models.py
@@ -1,3 +1,11 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+
+class Profile(models.Model):
+    id = models.AutoField(primary_key=True)
+    email = models.EmailField(unique=True)
+    nickname = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.email
